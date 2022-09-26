9efd7692adfbdb428539eabaef5fe3c14c66ab72
root <crusades97@gmail.com>
2022-09-26 12:56:52 +0300

    act_6

---
+++ pre_deploy.sh
@@ -0,0 +1,16 @@
+# Download the binary for your system
+sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+# Give it permission to execute
+sudo chmod +x /usr/local/bin/gitlab-runner
+
+# Create a GitLab Runner user
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+
+# Install and run as a service
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
+Once all is installed it’s time to register the runner against your project.
+
+sudo gitlab-runner register --url https://gitlab.com/ --registration-token GR1348941d3yvuDJDJMEKEIMDUwcLk
+
